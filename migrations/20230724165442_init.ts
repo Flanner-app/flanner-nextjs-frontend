b933ee35fed1f982eae2c6ca5aef08aa671cf10e
Oleks-Y <borith1111@gmail.com>
2023-07-24 19:17:31 +0200

    feat: add initial migration

---
+++ migrations/20230724165442_init.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema
+    .withSchema('public')
+    .createTable('users', (table) => {
+      table.uuid('id').defaultTo(knex.fn.uuid()).primary();
+      table.string('user_id').notNullable();
+    })
+    .createTable('fridges', (table) => {
+      table.uuid('id').defaultTo(knex.fn.uuid()).primary();
+      table.uuid('fridge_id').notNullable();
+      table.uuid('user_id').notNullable().references('id').inTable('users');
+    })
+    .createTable('items', (table) => {
+      table.uuid('id').defaultTo(knex.fn.uuid()).primary();
+      table.uuid('fridge_id').notNullable().references('id').inTable('fridges');
+      table.string('name').notNullable();
+      table.text('description').notNullable();
+      table.string('quantity').notNullable(); // for now will be just string
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {}
