c285bf2a01d7b83b2a886abc937e740609a0c14b
Whisil <electromandarin@gmail.com>
2023-07-30 13:26:17 +0300

    feat: add button

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -2,14 +2,40 @@ import clsx from 'clsx'
 import { ReactNode } from 'react'
 
 type ButtonProps = {
+  appearence: 'solid' | 'outline' | 'ghost'
   className?: string
   children: ReactNode
 }
 
-const Button = ({ className, children, ...restProps }: ButtonProps) => {
+const getClasses = (appearence: ButtonProps['appearence']) => {
+  switch (appearence) {
+    case 'solid':
+      return ['bg-primary-regular text-white', 'hover:bg-primary-hover']
+    case 'outline':
+      return [
+        'bg-transparent border border-primary-regular text-black',
+        'hover:bg-primary-hover hover:text-white',
+      ]
+    case 'ghost':
+      return ['bg-transparent', 'hover:bg-primary-hover hover:text-white']
+    default:
+      return []
+  }
+}
+
+const Button = ({
+  appearence,
+  className,
+  children,
+  ...restProps
+}: ButtonProps) => {
   return (
     <button
-      className={clsx('rounded-xl bg-violet-600 p-4 text-white', className)}
+      className={clsx(
+        'rounded-xl p-4 font-medium transition-colors',
+        className,
+        getClasses(appearence),
+      )}
       {...restProps}
     >
       {children}
