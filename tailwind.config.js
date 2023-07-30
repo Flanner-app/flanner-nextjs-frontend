8518ab9f1310b56d9a95e47f1c4d9140e385bdcf
Whisil <electromandarin@gmail.com>
2023-07-30 12:52:21 +0300

    chore: add temporary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,31 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      bg: 'rgba(248, 242, 225, 1)',
+      primary: {
+        light: '#d1beda',
+        dark: '#490572',
+        hover: '#693589',
+        regular: '983794',
+      },
+      secondary: {
+        dark: '#351D4F',
+        light: '#eeebf8',
+      },
+      gray: {
+        100: '#f8f8f8',
+        200: '#DDDDDD',
+        300: '#AAAAAA',
+        400: '#737373',
+        500: '#444444',
+        600: '#222222',
+      },
+      utility: {
+        error: '#d0011b',
+        success: '#5fb846',
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
