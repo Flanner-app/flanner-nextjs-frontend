0c95ce1a59fc012606d4c5937acbf873a2668ec7
Whisil <electromandarin@gmail.com>
2023-10-22 21:22:33 +0300

    repo rebase commit

--- src/app/(withSidebars)/page.tsx
+++ src/app/(withSidebars)/page.tsx
@@ -1,5 +1,16 @@
+import clsx from 'clsx'
+
 const Home = () => {
-  return <main className="grow p-8"></main>
+  return (
+    <main className="grow p-8">
+      <label
+        className={clsx(
+          'block h-10 w-24 rounded-xl border-2 border-black-default bg-white px-4 py-2',
+          'shadow-brutalism',
+        )}
+      ></label>
+    </main>
+  )
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -75,6 +75,7 @@ module.exports = {
       }),
       boxShadow: {
         outlined: '0 0 10px 2px rgba(0,0,0,0.1)',
+        brutalism: '3px 3px 0px 0px rgba(0,0,0,1)',
       },
     },
   },
