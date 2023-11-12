85d74ccde5ffbe3d9a604ec272addb5165e98f8b
Whisil <electromandarin@gmail.com>
2023-11-12 22:29:47 +0200

    chore: update tag classes

--- src/components/blog/Tag.tsx
+++ src/components/blog/Tag.tsx
@@ -12,7 +12,7 @@ const Tag = ({ type = 'regular', className, children }: TagProps) => {
   return (
     <div
       className={clsx(
-        'w-fit rounded-md px-3 py-1.5 text-sm font-medium leading-none',
+        'w-fit whitespace-nowrap rounded-md px-3 py-1.5 text-center text-sm font-medium',
         {
           [getRandomBgColor({ bright: true })]: type === 'regular',
           'flex items-center gap-1 bg-white bg-opacity-70 backdrop-blur-md':
